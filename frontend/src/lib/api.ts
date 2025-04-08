59636c25a7b1f89a0e89f82598681a481a7ce3c2
Docitec <romain.denis@docitec.com>
2025-04-08 17:37:55 +0200

    Mise à jour Connexion avec frontend

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,10 +1,10 @@
 import axios from "axios";
 
-const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+const baseURL = process.env.NEXT_PUBLIC_API_BASE_URL!;
+
+export const api = axios.create({
+  baseURL,
   headers: {
     "Content-Type": "application/json",
   },
 });
-
-export default api;
